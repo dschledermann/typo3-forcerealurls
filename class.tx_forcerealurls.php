d994675ee221434836a0de3941e9dd6601d87163
Daniel Schledermann <daniel@linkfactory.dk>
2015-05-26 13:24:47 +0200

    [BUGFIX] Replacing // -> / in the end of the url.

--- class.tx_forcerealurls.php
+++ class.tx_forcerealurls.php
@@ -4,44 +4,48 @@ class tx_forcerealurls {
 
 	function check($params,$pObj) {
 		if ($pObj->siteScript && $pObj->config['config']['tx_realurl_enable'] && (
-            substr($pObj->siteScript, 0, 9) == 'index.php' || 
-            substr($pObj->siteScript, 0, 1) == '?'
-        )) {
-            $extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['forcerealurls']);
+			substr($pObj->siteScript, 0, 9) == 'index.php' ||
+			substr($pObj->siteScript, 0, 1) == '?'
+		)) {
+			$extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['forcerealurls']);
 
-            // Skipping section
-            if ($extConf['skip_if_logged_in'] && $GLOBALS['BE_USER']->user) {
-                return;
-            }
+			// Skipping section
+			if ($extConf['skip_if_logged_in'] && $GLOBALS['BE_USER']->user) {
+				return;
+			}
 
-            $query = array();
-            parse_str(t3lib_div::getIndpEnv('QUERY_STRING'), $query);
+			$query = array();
+			parse_str(t3lib_div::getIndpEnv('QUERY_STRING'), $query);
 
-            if ($extConf['skip_if_no_cache'] && $query['no_cache']) {
-                return;
-            }
+			if ($extConf['skip_if_no_cache'] && $query['no_cache']) {
+				return;
+			}
 
-            if ($extConf['skip_if_typeNum'] && $query['type']) {
-                return;
-            }
+			if ($extConf['skip_if_typeNum'] && $query['type']) {
+				return;
+			}
 
-            // Stripping section
-            $strip_params = explode(' ', $extConf['strip_params']);
-            foreach($strip_params as $strip_param) {
-                unset ($query[$strip_param]);
-            }
+			// Stripping section
+			$strip_params = explode(' ', $extConf['strip_params']);
+			foreach($strip_params as $strip_param) {
+				unset ($query[$strip_param]);
+			}
 
-            $queryString = http_build_query($query);
+			$queryString = http_build_query($query);
 
-            $baseURL = $pObj->config['config']['baseURL'];
-            $LD = $pObj->tmpl->linkData($pObj->page,'',false,'','',$queryString);
-            $url = $baseURL.$LD['totalURL'];
+			$baseURL = $pObj->config['config']['baseURL'];
+			$LD = $pObj->tmpl->linkData($pObj->page,'',false,'','',$queryString);
+			$url = $baseURL.$LD['totalURL'];
 
-            header('HTTP/1.1 301 Moved Permanently');
-            header('Location: '.$url);
-            header('X-Redirected-By: forcerealurls');
+			// Cleanup for some reason sometimes two / are used in the end.
+			// Removing this
+			$url = preg_replace("|//$|", "/", $url);
 
-            exit;	
+			header('HTTP/1.1 301 Moved Permanently');
+			header('Location: '.$url);
+			header('X-Redirected-By: forcerealurls');
+
+			exit;
 		}
 	}
 }
